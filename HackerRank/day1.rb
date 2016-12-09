2b8cd0725f2f95122f5d17f598b4ab375411cc58
JB <volpe-jb@users.noreply.github.com>
2016-12-09 17:26:05 -0600

    Create day1.rb
    https://www.hackerrank.com/domains/tutorials/30-days-of-code

---
+++ HackerRank/day1.rb
@@ -0,0 +1,24 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# declare second integer, double and string variables
+j = 0
+e = 0.0
+t = ''
+
+# read and save second integer, double and string variables - NO PROMPTS
+j = gets
+
+e = gets
+
+t = gets
+
+# print the sum of both the integer variables on a new line
+puts (i.to_i + j.to_i)
+
+# print the sum of both the float variables on a new line
+puts (d.to_f + e.to_f)
+
+# concatenate and print the String variables on a new line (s var first)
+puts s + t
