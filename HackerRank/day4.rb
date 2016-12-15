90b4a65a4e6fd793b162b90c27bbaed7e7c7b61c
JB <volpe-jb@users.noreply.github.com>
2016-12-15 00:17:03 -0600

    Create day4.rb

---
+++ HackerRank/day4.rb
@@ -0,0 +1,36 @@
+class Person
+    attr_accessor :age
+    def initialize(initialAge)
+        if
+          initialAge !<= 0
+          age = initialAge
+        else
+          puts 'Age is not valid, setting age to 0.'
+          age = 0
+    end
+    def amIOld()
+      # Do some computations in here and print out the correct statement to the console
+      if age < 13
+        puts 'You are young.'
+          elsif age >= 13 && age < 18
+           puts 'You are a teenager.'
+             else
+               puts 'You are old.'
+       end   
+    end
+    def yearPasses()
+      age += 1
+    end
+end
+
+T=gets.to_i
+for i in (1..T)do
+    age=gets.to_i
+    p=Person.new(age)
+    p.amIOld()
+    for j in (1..3)do
+        p.yearPasses()
+    end
+    p.amIOld
+  	puts ""
+end      
