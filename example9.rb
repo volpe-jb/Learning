82c37fd320e5c041c82908480ce126b6a8caad65
JB <volpe-jb@users.noreply.github.com>
2017-01-01 17:54:10 -0600

    Create example9.rb
    Learn Ruby the Hard Way

---
+++ example9.rb
@@ -0,0 +1,16 @@
+# example9 Learn Rubby the Hard Way
+
+# here's some new strange stuff
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+    There's something going on here.
+    With the three double-quotes.
+    We'll be able to type as much as we like.
+    Even 4 lines if we want, or 5, or 6.
+}
