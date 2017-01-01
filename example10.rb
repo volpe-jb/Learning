a7ebc78b43cc8eb3ea3ee52b55afbed7ee55961b
JB <volpe-jb@users.noreply.github.com>
2017-01-01 17:53:22 -0600

    Create example10.rb
    Learn Ruby the Hard Way

---
+++ example10.rb
@@ -0,0 +1,36 @@
+# example10 Learn Ruby the Hard Way
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+little_dog = """
+Here's a dog list:
+\t*Dog bones
+\t*Dog food
+\t*ball
+"""
+name = 'Captain'
+bird = '''
+pellets
+#{name}
+'''
+
+bird2 = """
+#{name}
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts little_dog
+puts bird
+puts bird2
